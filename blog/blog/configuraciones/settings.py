cb9af28db06bf37da75743ab5fb5db93af73f2a7
Ezequiel <stambuliezequiel@gmail.com>
2026-03-08 20:43:57 -0300

    Cambio de rutas en settings

--- blog/blog/configuraciones/settings.py
+++ blog/blog/configuraciones/settings.py
@@ -81,10 +81,13 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -134,7 +137,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 # WhiteNoise maneja el CSS
-MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # --- CONFIGURACIÓN DE MEDIA (Imágenes de Perfumes) ---
